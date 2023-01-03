2ccae3e6c1102043c9f268df1d54bab98b2f717b
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-03 15:59:33 +0000

    added home route

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const PORT = process.env.PORT || 3005;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", function (request, response) {
+  response.json({
+    "/pharmcies": "retruns an array of pharmacies in a specific area",
+    "/hospitals": "retruns an array of hospitals in a specific area",
+    "/doctors": "retruns an array of doctors in a specific area",
+  });
+});
 
 app.listen(PORT, function () {
   console.log("Your app is listening on port " + PORT);
-})
+});
