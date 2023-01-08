b0639ac2247898908875228c71b7a852ea43b8a2
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-08 15:47:39 +0000

    added error message

--- client/src/App.js
+++ client/src/App.js
@@ -2,56 +2,56 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [city, setCity] = useState("harrow");
-  const [category, setCategory] = useState("doctors");
+  const [city, setCity] = useState("");
+  const [category, setCategory] = useState("");
   const [cityData, setCityData] = useState([]);
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
-    fetch(`/${city}/${category}`)
-      .then((res) => res.json())
-      .then((data) => setCityData(data));
+    city &&
+      category &&
+      fetch(`/${city}/${category}`)
+        .then((res) => res.json())
+        .then((data) => setCityData(data));
   }, [city, category]);
+
+  function selectCity(e) {
+    setShowMessage(false);
+    setCity(e.target.value.toLowerCase());
+  }
+
+  function selectCategory(e) {
+    if (city) {
+      setShowMessage(false);
+      setCategory(e.target.value.toLowerCase());
+    } else {
+      setCategory("");
+      setShowMessage(true);
+    }
+  }
 
   return (
     <div className="App">
       <h1>London City Guide</h1>
       <label htmlFor="cities">Choose a city:</label>
-      <select
-        id="cities"
-        name="cities"
-        onChange={(e) => setCity(e.target.value.toLowerCase())}
-      >
+      <select id="cities" name="cities" onChange={selectCity}>
+        <option value="-1">Select a city</option>
         <option value="stratford">Stratford</option>
         <option value="heathrow">Heathrow</option>
         <option value="harrow">Harrow</option>
       </select>
+      {showMessage && <div>Please select a city first</div>}
       <div>
-        <button
-          type="button"
-          value="pharmacies"
-          onClick={(e) => setCategory(e.target.value.toLowerCase())}
-        >
+        <button type="button" value="pharmacies" onClick={selectCategory}>
           Pharmacies
         </button>
-        <button
-          type="button"
-          value="doctors"
-          onClick={(e) => setCategory(e.target.value.toLowerCase())}
-        >
+        <button type="button" value="doctors" onClick={selectCategory}>
           Doctors
         </button>
-        <button
-          type="button"
-          value="hospitals"
-          onClick={(e) => setCategory(e.target.value.toLowerCase())}
-        >
+        <button type="button" value="hospitals" onClick={selectCategory}>
           Hospitals
         </button>
-        <button
-          type="button"
-          value="colleges"
-          onClick={(e) => setCategory(e.target.value.toLowerCase())}
-        >
+        <button type="button" value="colleges" onClick={selectCategory}>
           Colleges
         </button>
       </div>
